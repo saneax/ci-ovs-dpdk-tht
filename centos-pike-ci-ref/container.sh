b2613fe0b337cc3c570578202fab965046b1b017
Sanjay Upadhyay <supadhya@redhat.com>
2017-11-15 17:54:55 +0530

    working THT templates with 2 nics

--- centos-pike-ci-ref/container.sh
+++ centos-pike-ci-ref/container.sh
@@ -1,8 +1,53 @@
 #!/bin/bash
 
-openstack overcloud container image prepare \
-  --namespace 192.168.24.1:8787/master \
-  --tag latest \
-  --env-file ./docker_registry.yaml
+set -eux
 
-echo "  DockerInsecureRegistryAddress: 192.168.24.1:8787" >>  ./docker_registry.yaml
+### --start_docs
+## Prepare the undercloud for deploying the containerized compute node
+## ===================================================================
+
+## .. note:: In progress documentation is available at https://etherpad.openstack.org/p/tripleo-containers-work
+## ::
+
+## Prepare Your Environment
+## ------------------------
+
+## * Add an additional insecure registry if needed
+
+## * get build id
+## ::
+BUILD_ID=latest
+
+## * Populate the docker registry
+## ::
+
+openstack overcloud container image prepare \
+    --images-file ./overcloud_containers.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
+    --namespace docker.io/tripleoupstream \
+    --tag $BUILD_ID \
+    --push-destination 192.168.24.1:8787
+
+openstack overcloud container image upload --verbose --config-file ./overcloud_containers.yaml
+
+
+## * Configure the /home/stack/containers-default-parameters.yaml, this is done automatically.
+## ::
+
+openstack overcloud container image prepare \
+    --env-file ./containers-default-parameters.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
+    --namespace 192.168.24.1:8787/tripleoupstream \
+    --tag $BUILD_ID
+
+echo "============================="
+echo "Containers default parameters:"
+cat ./containers-default-parameters.yaml
+echo "============================="
+
+## * Get the journal logs for docker
+## ::
+
+sudo journalctl -u docker > docker_journalctl.log
+
+### --stop_docs
--- centos-pike-ci-ref/deploy.sh
+++ centos-pike-ci-ref/deploy.sh
@@ -5,17 +5,17 @@
 openstack overcloud deploy \
     --templates \
     -r roles_data.yaml \
-    --timeout 90 \
+    --timeout 60 \
     -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/low-memory-usage.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
+    -e containers-default-parameters.yaml \
     -e registry.yaml \
     -e dpdk-environment.yaml \
     -e common-environment.yaml \
     -e network-environment.yaml \
-    -e docker_registry.yaml \
     -e temp-env.yaml
 
 # Post Deploy Workarounds
